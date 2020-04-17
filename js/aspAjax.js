ac78aaa6f327e9b5932c7b7d01dbcf0b6481264e
Pieter Cooreman <47208869+PieterCooreman@users.noreply.github.com>
2020-04-18 00:00:22 +0200

    reorganize JavaScript code

--- js/aspAjax.js
+++ js/aspAjax.js
@@ -1,3 +1,5 @@
+var aspAjaxUrl='demo.asp'
+
 function aspAjax (type,url,dataType,data,success) {	
 
 	$.ajax({		
@@ -7,10 +9,62 @@ function aspAjax (type,url,dataType,data,success) {
 		data: data,
 		success: success,
 		error: aspAjaxError,
-	});
-	
+	});	
 };
 
 function aspAjaxError(data) {
 	console.log (data);
 	};
+
+$(document).ready(function(e) {		
+	onPageLoad()
+})
+
+$('.aspAjax').click(function(e) {
+	e.preventDefault()
+	aspAjax('GET',aspAjaxUrl,'text','e=' + this.id,aspAjaxSuccess)	
+	scroll()		
+})
+
+$('.ajaxForm').submit(function(e) {
+	e.preventDefault()
+	aspAjax('POST',aspAjaxUrl,'text',$(this).serialize(),aspAjaxSuccess)
+	scroll()
+})
+
+function onPageLoad () { 
+	aspAjax('GET',aspAjaxUrl,'text','e=onload',aspAjaxSuccess)	
+}
+
+function aspAjaxSuccess(data) {
+	$('#body').html(data)			
+}
+
+function scroll() {
+	$('html,body').animate({scrollTop: $("#ajaxhandler").offset().top-100}, 'slow')	
+}
+
+$('.aspAjaxJSON').click(function(e) {
+	e.preventDefault()
+	aspAjax('GET',aspAjaxUrl,'json','e=' + this.id,jsonToHTML)
+})
+
+function jsonToHTML(data) {
+
+	var records=data.records
+
+	var output='<ul>'
+
+	for(var i = 0; i < records.length; i++) {
+	
+		var obj = records[i]
+		output+="<li>" + obj.sName + ' ('
+		output+=obj.iYear + ")</li>";
+		
+	}
+	
+	output+='</ul>'	
+	
+	$('#body').html(output)	
+	scroll()
+}
